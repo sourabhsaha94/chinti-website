1b5cd75c33aec71c99e022c90a8bcfd2bae4680e
Sourabh Saha <sousaha@amazon.com>
2020-08-04 16:29:22 -0700

    Updates

---
+++ precache-manifest.92db6019949c18e6f5c807e55a654e8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ae9354960bc7bcc46fa92b35620337",
+    "url": "/chinti-website/index.html"
+  },
+  {
+    "revision": "664956fb3494d9b4f4e6",
+    "url": "/chinti-website/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "d39ed47bc7e4273c21eb",
+    "url": "/chinti-website/static/css/main.a8307afd.chunk.css"
+  },
+  {
+    "revision": "664956fb3494d9b4f4e6",
+    "url": "/chinti-website/static/js/2.62164328.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/chinti-website/static/js/2.62164328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39ed47bc7e4273c21eb",
+    "url": "/chinti-website/static/js/main.d1d465b3.chunk.js"
+  },
+  {
+    "revision": "99ee7d4397dd8afaf4f8",
+    "url": "/chinti-website/static/js/runtime-main.dcfc67be.js"
+  }
+]);
